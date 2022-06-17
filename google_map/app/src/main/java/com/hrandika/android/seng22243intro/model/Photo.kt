bdf73b967b01e84c6e2f0660e92c7e2848bc36ac
Hirun Prabodhya <hirunprabodhya@gmail.com>
2022-06-17 20:16:14 +0530

    google map api

---
+++ google_map/app/src/main/java/com/hrandika/android/seng22243intro/model/Photo.kt
@@ -0,0 +1,3 @@
+package com.hrandika.android.seng22243intro.model
+
+data class Photo(var title: String, var thumbnailUrl: String, var url: String)
---
+++ google_map/app/src/main/java/com/hrandika/android/seng22243intro/model/User.kt
@@ -0,0 +1,3 @@
+package com.hrandika.android.seng22243intro.model
+
+data class User(val id: Number, val name: String, val username: String, val email: String)
